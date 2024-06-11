22112c0881ce59e58e3ac2cd50e4223588cd1066
codermahbub20 <mkrefat5@gmail.com>
2024-06-12 01:05:20 +0600

    generate client secret

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ const jwt = require('jsonwebtoken')
 const morgan = require('morgan')
 const port = process.env.PORT || 5000
 
+const stripe = require('stripe')(process.env.PAYMENT_SECRET_KEY )
+
 // middleware
 const corsOptions = {
   origin: ['http://localhost:5173', 'http://localhost:5174'],
@@ -116,6 +118,12 @@ async function run() {
       res.send(result)
     })
 
+    // Get User role
+    app.get('/user/:email',async (req,res)=>{
+      const email = req.params.email;
+      const result = await usersCollection.findOne({email});
+      res.send(result);
+    })
 
      // Save a room in database
      app.post('/rooms', verifyToken, async (req, res) => {
@@ -133,6 +141,22 @@ async function run() {
         .toArray()
       res.send(result)
     })
+
+
+    // Generate Client Secret
+    app.post('/create-payment-intent',verifyToken,async(req,res)=>{
+      const price = req.body
+      const amount = parseInt(price * 100)
+      if(!price || amount < 1) return
+      const {client_secret} = await stripe.paymentIntents.create({
+        amount: amount,
+        currency: 'usd',
+        payment_method_types: ['card']
+      })
+      res.send({clientSecret: client_secret})
+    })
+
+
 
     // Send a ping to confirm a successful connection
     await client.db('admin').command({ ping: 1 })
